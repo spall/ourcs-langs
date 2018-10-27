b80c0c86f09ac10f6d85d69718ac78b49cbbff63
Carlo Angiuli <carlo@carloangiuli.com>
2018-10-27 16:50:53 -0400

    Porting graphviz file to new format.

--- graphs/graphviz.rkt
+++ graphs/graphviz.rkt
@@ -1,12 +1,5 @@
 #lang racket
 
-
-(define simple-graph
-  '((istanbul . newyork)
-    (amsterdam . newyork)
-    (sydney . amsterdam)
-    (london . sydney)))
-
 ;; graphviz: Graph -> DotFileFormat
 (define (graphviz g)
   (define graph-init "graph graphname {")
@@ -15,20 +8,14 @@
   (string-append graph-init graph-inner graph-after))
 
 ;; extract-edges: Graph -> String
-;; ((a . b) (c . d)) -> "a -- b; c -- d"
+;; ((w a b) (w c d)) -> "a -- b; c -- d"
 (define (extract-edges g)
   (cond
     [(empty? g) ""]
     [else (string-append (generate-string (car g)) (extract-edges (cdr g)))]))
 
 ;; generate-string: Pair -> String
-;; (a . b) == "a -- b;"
+;; (w a b) == "a -> b;"
 (define (generate-string p)
-  (define first (car p))
-  (define second (cdr p))
-  (define first-string (symbol->string first))
-  (define second-string (symbol->string second))
-  (string-append first-string " -- " second-string "; "))
- 
+  (string-append (second p) " -> " (third p) "; "))
 
-(string-append "hi " "world")
