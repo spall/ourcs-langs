352b8c7c975fb70d1312105883471fbb65d31b38
Carlo Angiuli <carlo@carloangiuli.com>
2018-10-27 14:32:01 -0400

    Generate divisibility graphs...

--- divisibility.rkt
+++ divisibility.rkt
@@ -1,8 +1,32 @@
+#lang racket
+
+(require math/number-theory)
+
 ; Take in a graph representing information about divisibility...
 
 ; V = 1 .. n (all of them)
 ; E = {(n,m,w) | n,m in V, w is prime (not 1),
 ;                m * w = n}
+
+(define (write-divisibility-graph n)
+  (define (list->csv f ls)
+     (apply string-append (add-between (map f ls) ", ")))
+  (let*
+      ([o (open-output-file "div-graph" #:exists 'replace)]
+       [numbers (range 1 n)]
+       [edges (append-map
+               (lambda (n) (map (lambda (p) (list n (/ n p) p)) (prime-divisors n)))
+               numbers)])
+    (fprintf o "digraph divisibility~s { nodes : {" n)
+    (fprintf o (list->csv number->string numbers))
+    (fprintf o "}\n edges : {")
+    (fprintf o (list->csv
+               (lambda (l) (format "[ ~s ; ~s -> ~s ]" (third l) (first l) (second l)))
+               edges))
+    (fprintf o "\n")
+    (close-output-port o)))
+
+(write-divisibility-graph 50)
 
 ; property: w < n
 
